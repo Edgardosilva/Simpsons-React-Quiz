7132fae3c361b45c0cdc17bc15da1b556f959696
Edgardosilva <edgardosilva.es@gmail.com>
2023-11-17 15:48:22 -0300

    Question fixed

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import './game.css'
+import { preguntas } from './questions.js'
+import { useState } from "react";
 
 const Game = () => {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  
+
+  const handleAnswer = (selectAnswer) => {
+    if (selectAnswer === preguntas[currentQuestion].respuestaCorrecta) {
+      setScore(score + 1);
+    }
+    setCurrentQuestion(currentQuestion + 1)
+  }
+    
+
   return (
     <section className="gameLayout">
       <div className="questionNumber">
         PREGUNTA 1
       </div>
       <div className="Question">
-        ¿Quien mato al señor Burns?
+        {preguntas[currentQuestion].pregunta}
       </div>
       <div className="ulDiv">
         <ul>
-          <li>Respuesta 1</li>
-          <li>Respuesta 2</li>
-          <li>Respuesta 3</li>
-          <li>Respuesta 4</li>
+          {preguntas[currentQuestion].opciones.map((opcion, index) => 
+            <li key={index} onClick={() => handleAnswer(opcion)}>
+              {opcion}
+            </li>
+          )}
         </ul>
       </div>
     </section>
---
+++ src/pages/game/questions.js
@@ -0,0 +1,138 @@
+export const preguntas = [
+  {
+    pregunta: "¿Cuál es el nombre completo de Homero?",
+    respuestaCorrecta: "Homero Jay Simpson",
+    opciones: [
+      "Homero Yai Simpson",
+      "Homero J Simpson",
+      "Homero Jey Simpson",
+      "Homero Jay Simpson",
+    ],
+  },
+  {
+    pregunta: "¿Cuál es el nombre del bar de Moe?",
+    respuestaCorrecta: "Taberna de Moe",
+    opciones: ["Bar de Moe", "Moe's Tavern", "Moe's", "Cervecería de Moe"],
+  },
+  {
+    pregunta: "¿Quién es el director de la escuela primaria de Springfield?",
+    respuestaCorrecta: "Seymour Skinner",
+    opciones: [
+      "Saymour Skinner",
+      "Seymour Skinner",
+      "Barney Gumble",
+      "Apu Nahasapeemapetilon",
+    ],
+  },
+  {
+    pregunta: "¿Cuál es el nombre real de Ned Flanders?",
+    respuestaCorrecta: "Nedward Flanders",
+    opciones: ["Ned", "Nedward Flanders", "Nedson Flanders", "Flan Ned"],
+  },
+  {
+    pregunta: "¿Quién es el dueño del Kwik-E-Mart?",
+    respuestaCorrecta: "Apu Nahasapeemapetilon",
+    opciones: [
+      "Moe Szyslak",
+      "Barney Gumble",
+      "Apu Nahasapeemapetilon",
+      "Lenny Leonard",
+    ],
+  },
+  {
+    pregunta:
+      "¿Cuál es el nombre completo del jefe de Homero en la planta nuclear?",
+    respuestaCorrecta: "Charles Montgomery Burns",
+    opciones: [
+      "Montgomery Burns",
+      "Burns Montgomery",
+      "Charles Montgomery Burns",
+      "Sr. Burns",
+    ],
+  },
+  {
+    pregunta: "¿Cuál es el nombre de la hermana menor de Marge?",
+    respuestaCorrecta: "Patty",
+    opciones: ["Patty", "Selma", "Maggie", "Lisa"],
+  },
+  {
+    pregunta: "¿Cómo se llama el programa de televisión favorito de Bart?",
+    respuestaCorrecta: "El Show de Krusty",
+    opciones: [
+      "Padre de Familia",
+      "El Show de Krusty",
+      "The Itchy & Scratchy Show",
+      "Bob Esponja",
+    ],
+  },
+  {
+    pregunta: "¿Cuál es el nombre de la banda de Homer Simpson?",
+    respuestaCorrecta: "Los Borbotones",
+    opciones: [
+      "Los Descarriados",
+      "Los Borbotones",
+      "Los Barbotones",
+      "Los Rudos del Rock",
+    ],
+  },
+  {
+    pregunta:
+      "¿Cómo se llama el jefe de Bart en la planta de energía nuclear cuando trabaja medio tiempo?",
+    respuestaCorrecta: "Jimbo Jones",
+    opciones: [
+      "Jimbo Jones",
+      "Nelson Muntz",
+      "Dolph Starbeam",
+      "Kearney Zzyzwicz",
+    ],
+  },
+  {
+    pregunta: "¿Cuál es el nombre del perro de la familia Simpson?",
+    respuestaCorrecta: "Ayudante de Santa",
+    opciones: ["Rin Tin Tin", "Huesos", "Ayudante de Santa", "Bola de Nieve"],
+  },
+  {
+    pregunta: "¿Cómo se llama el robot que corrige la forma de hablar creado por Lisa?",
+    respuestaCorrecta: "Linguo",
+    opciones: [
+      "Jimbo",
+      "Linguo",
+      "Limbo",
+      "Languo",
+    ],
+  },
+  {
+    pregunta: "Termina la cancion: Probando mi amor por ti, probandome ...",
+    respuestaCorrecta: "Tu lealtad",
+    opciones: ["Tu amor", "Tu lealtad", "Tu prosperidad", "Tu pasion"],
+  },
+  {
+    pregunta:
+      "¿Cuál es el nombre del actor que dobla la voz de Homero en la versión original en inglés?",
+    respuestaCorrecta: "Dan Castellaneta",
+    opciones: [
+      "Harry Shearer",
+      "Dan Castellaneta",
+      "Hank Azaria",
+      "Yeardley Smith",
+    ],
+  },
+  {
+    pregunta: "¿Cómo se llama la ciudad vecina de Springfield?",
+    respuestaCorrecta: "Shelbyville",
+    opciones: ["Shelbyville", "Capital City", "Ogdenville", "North Haverbrook"],
+  },
+  {
+    pregunta:
+      "Termina la frase: No lloren por mi, yo ya ...",
+    respuestaCorrecta: "Edna Krabappel",
+    opciones: [
+      "Estoy muerto",
+      "Morí",
+      "Me fuí",
+      "Aprendí",
+    ],
+  },
+];
+
+
