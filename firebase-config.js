d7d1171fccc32df532fad49b60953d1d0cbc48d2
Edgardosilva <edgardosilva.es@gmail.com>
2024-01-24 19:27:17 -0300

    firebase config withoutt apikey env

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: "simpsonsquiz-7b196.firebaseapp.com",
+    projectId: "simpsonsquiz-7b196",
+    storageBucket: "simpsonsquiz-7b196.appspot.com",
+    messagingSenderId: "304554959604",
+    appId: "1:304554959604:web:0e18a3415db144fde974a1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const googleProvider = new GoogleAuthProvider();
+  export const db = getFirestore(app);
